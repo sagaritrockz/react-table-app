1b269eb4c91b4a20c4f1652f7f2807a7d2ebed96
Sagar Patel <sagaritrockz@users.noreply.github.com>
2018-09-24 10:57:19 +0200

    Update Select.js

--- src/Components/Elements/Select.js
+++ src/Components/Elements/Select.js
@@ -1,14 +1,15 @@
 import React from "react"
+import { array, number, string, func } from 'prop-types'
 import "./Select.css"
 
-const Select = ({ list, change, value, name, show }) => {
+const Select = ({ list, change, value, name, defaultOption }) => {
 
     const onChange = e => {
         e.preventDefault();
         change(e.target.name, e.target.value);
     };
     
-    if (show) {
+    if (defaultOption != '') {
         return (
             <select
                 className="select select-custom"
@@ -16,7 +17,7 @@ const Select = ({ list, change, value, name, show }) => {
                 name={name}
                 value={value}
             >
-                <option value={0}>Select</option>
+                <option value={0}>{defaultOption}</option>
                 {list.map((item, index) => (
                     <option key={index} value={item.id ? item.id : index + 1} >
                         {item.value ? item.value : item}
@@ -47,7 +48,15 @@ Select.defaultProps = {
     change: f => f, 
     value: '', 
     name: '',
-    show: false
-}
+    defaultOption: ''
+};
+
+Select.propTypes = {
+    list: array.isRequired,
+    change: func.isRequired, 
+    value: number.isRequired, 
+    name: string.isRequired,
+    defaultOption: string
+};
 
 export default Select;
